a26d7cc318274dfcda474e834f690a5d53b2cb3a
U539853 <bence.konecsni@lhsystems.com>
2016-11-11 12:55:01 +0100

    upcomingmatches skeleton

--- src/main/resources/static/src/client/app/UpcomingMatches.js
+++ src/main/resources/static/src/client/app/UpcomingMatches.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import Divider from 'material-ui/Divider';
+import MobileTearSheet from './MobileTearSheet';
+import TextField from 'material-ui/TextField';
 
 class UpcomingMatches extends React.Component {
 
@@ -18,31 +20,48 @@ class UpcomingMatches extends React.Component {
 
 	render() {
         var matchNodes = this.state.matches.map(function (match, i) {
-			var homeTeamName = match.homeTeamName; 
+			var homeTeamName = match.homeTeamName;
 			var awayTeamName = match.awayTeamName;
-			var homeTeamLogoSrc = "/images/" + homeTeamName.replace("/", "") + ".png";
-			var awayTeamLogoSrc = "/images/" + awayTeamName + ".png";
-			
-              return (
-				<div key={i}> 
-					<List >
-						<ListItem
-							primaryText = {homeTeamName}
-							rightAvatar = { <Avatar> {match.homeTeamGoals} </Avatar> }
-							leftAvatar = {<Avatar src = {homeTeamLogoSrc} />} />
-						<ListItem
-							primaryText = {awayTeamName}
-							rightAvatar = { <Avatar> {match.awayTeamGoals} </Avatar> }
-							leftAvatar = {<Avatar src = {awayTeamLogoSrc} />} />
-					</List>
-				    <Divider/>
-				</div>
-              );
+			var homeTeamLogoSrc = "/images/logos/" + homeTeamName + ".png";
+			var awayTeamLogoSrc = "/images/logos/" + awayTeamName + ".png";
+
+            return (
+				<List >
+					<ListItem
+						primaryText = {homeTeamName}
+						rightAvatar = { <Avatar> <TextField underlineShow={false} inputSytle={{textAlign:'center'}} hintText="Hint"/> </Avatar> }
+						leftAvatar = {<Avatar src = {homeTeamLogoSrc} />} />
+					<ListItem
+						primaryText = {awayTeamName}
+						rightAvatar = { <TextField hintText="Hint"/> }
+						leftAvatar = {<Avatar src = {awayTeamLogoSrc} />} />
+				</List>
+            );
         });
 
+		var mobileTearSheets = matchNodes.map(function (matchNode, i) {
+			var sheets = [];
+				if (i % 4 == 0) {
+					const sheet = (
+						<MobileTearSheet key={i}>
+							{matchNodes[i]}
+							<Divider/>
+							{matchNodes[i+1]}
+							<Divider/>
+							{matchNodes[i+2]}
+							<Divider/>
+							{matchNodes[i+3]}
+						</MobileTearSheet>
+					);
+					sheets.push(sheet);
+				}
+
+			return sheets;
+		});
+
         return (
-			<div>
-				{matchNodes}
+			<div style={{display: 'flex', flexWrap: 'wrap'}}>
+				{mobileTearSheets}
 			</div>
         );
 	}
